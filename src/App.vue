db5371d88209ae25426003a5ef22453ea955e250
saepes <tima123tima1@gmail.com>
2022-07-23 14:53:12 +0300

    feat: Working with Attributes in the Vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
- 
+  <a 
+    :href="href"
+  >
+  {{ text }}
+  </a>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      text: 'page',
+      href: 'page.html',
+    }
+  }
 }
 </script>
