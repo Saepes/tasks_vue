83ad7aa8808033376c3c9e480416bdbc1ceaa5d9
saepes <tima123tima1@gmail.com>
2022-07-23 20:27:08 +0300

    feat: Helper methods in Vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
 <template>
- 
+  <div>
+    <input type="text" v-model="day" />
+    <p>День недели - {{ showWeekDay(day) }}</p>
+  </div>
+
+  <div>
+    <p>Сегодня - {{ showWeekDayToday() }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+  name: "App",
+
+  data() {
+    return {
+      day: "",
+    };
+  },
+
+  methods: {
+    showWeekDay: function (day) {
+      return this.getWeekDay(this.day);
+    },
+
+    showWeekDayToday: function () {
+      let dateToday = new Date().getDate();
+      return this.getWeekDay(dateToday);
+    },
+
+    getWeekDay: function (day) {
+      let days = ["ВС", "ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ"];
+      let date = new Date(2022, 6, day);
+
+      return days[date.getDay()];
+    },
+  },
+};
 </script>
