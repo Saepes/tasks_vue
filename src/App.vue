5a88dd0fde66345277ab852d9534b219aacf08a5
saepes <tima123tima1@gmail.com>
2022-07-23 15:13:16 +0300

    feat: Code execution in Vue view

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
- 
+  <div>
+    <p>Квадрат числа - {{ num * num }}</p>
+    <p>Сумма чисел - {{ num1 + num2 + num3 }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+  name: "App",
+
+  data() {
+    return {
+      num: 5,
+      num1: 1,
+      num2: 2,
+      num3: 3,
+    };
+  },
+};
 </script>
