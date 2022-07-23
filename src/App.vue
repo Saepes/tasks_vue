b30c42b595a5dda0b93b51524be6024b52450009
saepes <tima123tima1@gmail.com>
2022-07-23 22:58:32 +0300

    feat: Reactivity in the Vue Framework

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,39 @@
 <template>
- 
+  <div>
+    <p>{{ text }}</p>
+    <button @click="changeProps()">Кнопка</button>
+  </div>
+
+  <div>
+    <button @click="changePropsBtn()">Кнопка 1</button>
+    <button @click="changePropsBtn2()">Кнопка 2</button>
+    <p>{{ text2 }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+  name: "App",
+
+  data() {
+    return {
+      text: "Hello, world",
+      text2: "",
+    };
+  },
+
+  methods: {
+    changeProps: function () {
+      this.text = "test";
+    },
+
+    changePropsBtn: function () {
+      this.text2 = "test btn 1";
+    },
+
+    changePropsBtn2: function () {
+      this.text2 = "test btn 2";
+    },
+  },
+};
 </script>
