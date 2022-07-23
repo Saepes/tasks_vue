b43c870605e1b7d4b4d658c04587c9b315f79deb
saepes <tima123tima1@gmail.com>
2022-07-23 15:30:17 +0300

    feat: Objects in Vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
- 
+  <div>
+    <p>Сумма чисел объекта - {{ obj.x + obj.y + obj.z }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+  name: "App",
+
+  data() {
+    return {
+      obj: { x: 1, y: 2, z: 3 },
+    };
+  },
+};
 </script>
