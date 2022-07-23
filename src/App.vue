95dddf3c30a0924a13d0d1e9bab05d324b72cf01
saepes <tima123tima1@gmail.com>
2022-07-23 15:22:26 +0300

    feat: Arrays in Vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <template>
- 
+  <div>
+    <p>
+      {{ arr[0] }}
+    </p>
+
+    <p>
+      {{ arr[1] }}
+    </p>
+
+    <p>
+      {{ arr[2] }}
+    </p>
+  </div>
+
+  <div>
+    <p>Сумма чисел массива - {{ arr2[0] + arr2[1] + arr2[2] }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+  name: "App",
+
+  data() {
+    return {
+      arr: ["x", "y", "z"],
+      arr2: [1, 2, 3],
+    };
+  },
+};
 </script>
