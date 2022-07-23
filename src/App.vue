d4a3cdcdce5bfc399137fa8d1138d2921ab7616c
saepes <tima123tima1@gmail.com>
2022-07-23 16:08:15 +0300

    feat: Working with data inside Vue methods

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
- 
+  {{ getSum() }}
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+  name: "App",
+
+  data() {
+    return {
+      num1: 1,
+      num2: 2,
+    };
+  },
+
+  methods: {
+    getSum: function () {
+      alert(this.num1 + this.num2);
+    },
+  },
+};
 </script>
