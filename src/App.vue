80a812723bd63beae5f9fd52c685b969e661dadb
saepes <tima123tima1@gmail.com>
2022-07-23 15:57:42 +0300

    feat: Hanging event handlers in Vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
- 
+  <div>
+    <button @click="getDate">Кнопка</button>
+
+    <button @mouseover="getDate">Кнопка</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+  name: "App",
+
+  methods: {
+    getDate: () => {
+      let date = new Date().toISOString().slice(0, 10);
+      return alert(date);
+    },
+  },
+};
 </script>
