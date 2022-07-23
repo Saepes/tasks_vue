31eeea15cb715ed4077cae9270adc5419a63abd9
saepes <tima123tima1@gmail.com>
2022-07-23 14:34:02 +0300

    feat: Added example component structure vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Задание 1</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officiis, delectus labore pariatur placeat dolorem. Quo aliquid dolorum sapiente? Doloribus tempore expedita nulla quasi laboriosam! Voluptatibus dolorem non veniam quam?</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
